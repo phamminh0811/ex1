15b6552d81d2f80021ab275ccbc1aebf244f50c1
phamminh0811 <1phamminh0811@gmail.com>
2022-06-24 00:57:40 +0700

    Almost finish dashboard

---
+++ dashboard/dashboard.js
@@ -0,0 +1,138 @@
+// [   
+//     {
+//         "device": "TV",
+//         "created_date": "2022-06-18",
+//         "power_consume": 50
+//     },
+//     {
+//         "device": "Washer",
+//         "created_date": "2022-06-19",
+//         "power_consume": 60
+//     },
+//     {
+//         "device": "Refrigerator",
+//         "created_date": "2022-06-20",
+//         "power_consume": 80
+//     },
+//     {
+//         "device": "Selling Fan",
+//         "created_date": "2022-06-21",
+//         "power_consume": 100
+//     }
+// ]
+
+async function addDevice() {
+    let form = document.forms["add_device"];
+    let device_name = form["name"].value;
+    let device_pow_consume = form["power_consume"].value;
+    if (isNaN(device_pow_consume)) {
+        alert("Must input numbers");
+    }
+    
+    try {
+    fetch('./dashboard.json')
+        .then(response => response.json())
+        .then(function(data){
+            var devices = data;
+            devices.push({
+                device: device_name,
+                created_date: getCurrDate(),
+                power_consume: device_pow_consume
+            });
+            console.log(devices);
+            fetch('./dashboard.json',{
+                method: 'POST',
+                body: JSON.stringify(devices),
+            });
+        });
+    } catch (e) {
+        console.log('try-catch')
+        console.log(e)
+    }
+        
+}
+
+function getCurrDate() {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+
+    return yyyy + '-' + mm + '-' + dd;
+}
+
+function showDevices() {
+    fetch('./dashboard.json')
+        .then(response => response.json())
+        .then(data => {
+            var devices = data;
+            var col = [];
+            for (var i = 0; i < devices.length; i++) {
+                for (var key in devices[i]) {
+                    if (col.indexOf(key) === -1) {
+                        col.push(key);
+                    }
+                }
+            }
+
+
+            var tbody_tag = document.createElement("tbody");
+            var total = 0, tr;
+
+            for (var i = 0; i < devices.length; i++) {
+
+                tr = tbody_tag.insertRow(-1);
+
+
+                var tabCell = tr.insertCell(-1);
+                tabCell.innerHTML = devices[i].device;
+                var tabCell = tr.insertCell(-1);
+                tabCell.innerHTML = "00:18:44:11:3A:87";
+                var tabCell = tr.insertCell(-1);
+                tabCell.innerHTML = "127.0.0.2";
+                var tabCell = tr.insertCell(-1);
+                tabCell.innerHTML = devices[i].created_date;
+                var tabCell = tr.insertCell(-1);
+                tabCell.innerHTML = devices[i].power_consume;
+                total += devices[i].power_consume;
+            }
+            console.log(tbody_tag)
+            document.getElementById("table").appendChild(tbody_tag);
+            document.getElementById("total_consume").innerText = total;
+        });
+}
+
+function showDevicesDashboard() {
+    fetch('./dashboard.json')
+        .then(response => response.json())
+        .then(data => {
+            var devices = data;
+            var devices_ec = [];
+            for (var i = 0; i < devices.length; i++) {
+                devices_ec.push({
+                    y: devices[i].power_consume,
+                    legendText: devices[i].device + " " + devices[i].power_consume,
+                    label: devices[i].device
+                })
+            }
+            var chart = new CanvasJS.Chart("dashboard-display", {
+                animationEnabled: true,
+                title: {
+                    horizontalAlign: "left"
+                },
+                data: [{
+                    type: "doughnut",
+                    startAngle: 0,
+                    innerRadius: 50,
+                    indexLabelFontSize: 10,
+                    toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+                    dataPoints: devices_ec,
+                    showInLegend: true,
+                }]
+            });
+            chart.render();
+    });
+  
+}
+
